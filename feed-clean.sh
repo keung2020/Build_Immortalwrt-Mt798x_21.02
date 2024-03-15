4b1cdb976a79bc4eb6f5441db9ef8eea45331c9d
keung2020 <62239958+keung2020@users.noreply.github.com>
2024-03-15 15:24:00 +0800

    Update feed-clean.sh

--- feed-clean.sh
+++ feed-clean.sh
@@ -26,62 +26,22 @@ function merge_package() {
 }
 
 #Delete duplicate file
-rm -rf feeds/luci/applications/luci-app-aliddns
-rm -rf feeds/luci/applications/luci-app-autoreboot
-rm -rf feeds/luci/applications/luci-app-baidupcs-web
-rm -rf feeds/luci/applications/luci-app-beardropper
-rm -rf feeds/luci/applications/luci-app-dockerman
-rm -rf feeds/luci/applications/luci-app-fileassistant
-rm -rf feeds/luci/applications/luci-app-filebrowser
-rm -rf feeds/luci/applications/luci-app-gost
-rm -rf feeds/luci/applications/luci-app-guest-wifi
-rm -rf feeds/luci/applications/luci-app-iptvhelper
-rm -rf feeds/luci/applications/luci-app-netdata
-rm -rf feeds/luci/applications/luci-app-onliner
-rm -rf feeds/luci/applications/luci-app-openclash
-rm -rf feeds/luci/applications/luci-app-openvpn-server
 rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/luci/applications/luci-app-pppoe-relay
-rm -rf feeds/luci/applications/luci-app-pppoe-server
-rm -rf feeds/luci/applications/luci-app-ramfree
-rm -rf feeds/luci/applications/luci-app-serverchan
-rm -rf feeds/luci/applications/luci-app-smartdns
-rm -rf feeds/luci/applications/luci-app-sms-tool
-rm -rf feeds/luci/applications/luci-app-socat
-rm -rf feeds/luci/applications/luci-app-softethervpn
-rm -rf feeds/luci/applications/luci-app-ssr-mudb-server
 rm -rf feeds/luci/applications/luci-app-ssr-plus
-rm -rf feeds/luci/applications/luci-app-syncthing
-rm -rf feeds/luci/applications/luci-app-sysuh3c
-rm -rf feeds/luci/applications/luci-app-timecontrol
-rm -rf feeds/luci/applications/luci-app-udp2raw
-rm -rf feeds/luci/applications/luci-app-unblockneteasemusic
-rm -rf feeds/luci/applications/luci-app-usb3disable
-rm -rf feeds/luci/applications/luci-app-verysync
-rm -rf feeds/luci/applications/luci-app-vssr
-rm -rf feeds/luci/applications/luci-app-watchcat
-rm -rf feeds/luci/applications/luci-app-wrtbwmon
-rm -rf feeds/luci/applications/luci-app-xlnetacc
-rm -rf feeds/luci/applications/luci-app-xunlei
 rm -rf feeds/luci/applications/luci-theme-argon
 #rm -rf feeds/packages/net/naiveproxy
 #rm -rf feeds/packages/net/xray-core
 #rm -rf feeds/packages/net/xray-plugin
-rm -rf feeds/packages/net/brook
-rm -rf feeds/packages/net/chinadns-ng
-rm -rf feeds/packages/net/dns2socks
-rm -rf feeds/packages/net/dns2tcp
-rm -rf feeds/packages/net/hysteria
-rm -rf feeds/packages/net/smartdns
+#rm -rf feeds/packages/net/brook
+#rm -rf feeds/packages/net/chinadns-ng
+#rm -rf feeds/packages/net/dns2socks
+#rm -rf feeds/packages/net/dns2tcp
+#rm -rf feeds/packages/net/hysteria
+#rm -rf feeds/packages/net/smartdns
 #rm -rf feeds/packages/net/shadowsocks-rust
-rm -rf /feeds/smpackage/shadowsocks-rust
-# rm -rf feeds/packages/net/mosdns
-rm -rf /feeds/smpackage/mosdns
-# rm -rf feeds/luci/applications/luci-app-adbyby-plus
-# merge_package openwrt-23.05 https://github.com/immortalwrt/luci feeds/luci/applications applications/luci-app-adbyby-plus
+#rm -rf feeds/packages/net/mosdns
 #rm -rf feeds/packages/net/frp
-#merge_package openwrt-23.05 https://github.com/immortalwrt/packages feeds/packages/net net/frp
-# svn co https://github.com/immortalwrt/packages/branches/openwrt-23.05/net/frp feeds/packages/net/frp
+merge_package main https://github.com/kenzok8/small-package feeds/luci/applications luci-app-bypass
 #rm -rf feeds/packages/lang/golang
 # git clone https://github.com/openwrt/packages/tree/openwrt-23.05/lang/golang feeds/packages/lang/golang
 #merge_package openwrt-23.05 https://github.com/immortalwrt/packages feeds/packages/lang lang/golang
